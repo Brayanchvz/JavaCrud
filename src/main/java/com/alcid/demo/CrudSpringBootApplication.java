4c202631b06e11fa61a67de32a53959e639a9dc7
Brayanchvz <49880769+Brayanchvz@users.noreply.github.com>
2021-11-06 15:53:35 -0600

    Deploy project

---
+++ src/main/java/com/alcid/demo/CrudSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.alcid.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/alcid/demo/modelo/Persona.java
@@ -0,0 +1,5 @@
+package com.alcid.demo.modelo;
+
+public class Persona {
+
+}
